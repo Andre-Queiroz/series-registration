0f0341302e33ddc37e48d3d87dc01025f81ace7b
Andre-Queiroz <andretorresdequeiroz@gmail.com>
2021-08-20 01:30:16 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace series_registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
